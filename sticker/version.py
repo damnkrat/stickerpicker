3beec234b028110d5721d95fc1c365c56e7e4b1b
damnkrat <damnkrat@gmail.com>
2025-10-25 13:09:55 +0300

    Add nikita stickers

--- sticker/version.py
+++ sticker/version.py
@@ -1,1 +1,6 @@
-from .get_version import git_tag, git_revision, version, linkified_version
+# Generated in setup.py
+
+git_tag = None
+git_revision = '3366dbc5'
+version = '0.1.0+dev.3366dbc5'
+linkified_version = '0.1.0+dev.[3366dbc5](https://github.com/maunium/stickerpicker/commit/3366dbc5002046be058a71e7ed310811a122c081)'
